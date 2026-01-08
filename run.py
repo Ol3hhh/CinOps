b51838b26cf6ff7f559d836342fc1fe959197dca
Ol3hhh <olehmaru@gmail.com>
2026-01-08 22:38:25 +0100

    frontend setup (dockerfile, compose, installed react)

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-# run.py
 from dotenv import load_dotenv
 import os
 from backend import create_app
@@ -8,11 +7,9 @@ load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
-    # --- DODAJ TO: ---
     print("\n--- DOSTĘPNE ADRESY (ROUTES) ---")
     for rule in app.url_map.iter_rules():
         print(f"{rule.endpoint}: {rule}")
     print("--------------------------------\n")
-    # -----------------
     
     app.run(debug=True, host="0.0.0.0", port=5000)
